125c2549a39c648d7350f334d58a6af56541a9c1
zoeytra <zoeytra@gmail.com>
2021-10-05 14:48:01 +0800

    第三次

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@ module.exports = {
   mode: 'jit',
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'fb-bg': '#18191a',
+        'fb-header': '#242526',
+        'fb-input': '#3a3b3c',
+        'fb-popover': '#3e4042',
+        'fb-active': '#323436',
+        'fb-card': '#242526',
+        'fb': '#2e89ff'
+      }
+
+      }
+    },
   },
   variants: {
     extend: {},
